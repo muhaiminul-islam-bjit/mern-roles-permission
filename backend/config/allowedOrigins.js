72d6881d18797bdac39d11d3f44e516b684773d7
muhaiminulIslamIkhlas <muhaiming2c@gmail.com>
2023-01-08 23:51:06 +0600

    fix cors origin issue

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://www.google.com'
+    'https://www.google.com',
+    'http://139.59.13.204:3000'
 ]
 
 module.exports = allowedOrigins;
